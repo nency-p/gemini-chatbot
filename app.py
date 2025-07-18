44dbe2c268ed26e4b9d764c781d1818a3409c70f
nency-p <patnency07@gmail.com>
2025-07-18 21:52:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st # type: ignore
 import google.generativeai as genai # type: ignore
 
 # Load API key from Streamlit secrets
-genai.configure(api_key=st.secrets["AIzaSyCgjmJOfjcbupwqL3Nm79sFC6aHCrgJQmY"])
+genai.configure(api_key=st.secrets["AIzaSyBrdDhLHxumSpMerAmLkbE7lWoUG4dEuTY"])
 
 # Initialize the Gemini model
 model = genai.GenerativeModel("gemini-pro")
